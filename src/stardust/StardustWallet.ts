9e68b463fbec663d1127322424f3dbdbd5b0a4f5
andrew <andrew.gutierrez@stardust.gg>
2023-03-02 11:29:25 -0800

    use yarn

--- src/stardust/StardustWallet.ts
+++ src/stardust/StardustWallet.ts
@@ -3,13 +3,12 @@ import StardustApp from './StardustApp';
 
 export default class StardustWallet {
   readonly _name: string = 'StardustWallet';
-  private constructor(
-    private readonly _parent: StardustApp,
-    public readonly id: string
-  ) {}
+  private constructor(private readonly _parent: StardustApp, public readonly id: string) {}
 
   static async create(parent: StardustApp): Promise<StardustWallet> {
-    const response = await axios.post(`${parent.getUrl()}/wallet`, null, { headers: { 'x-api-key': parent.getApiKey() } });
+    const response = await axios.post(`${parent.getUrl()}/wallet`, null, {
+      headers: { 'x-api-key': parent.getApiKey() },
+    });
     const { id } = response.data;
     return new StardustWallet(parent, id);
   }
