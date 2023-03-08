eb72024854f7161bd5b8d2b47691f5d91d9cdcd1
andrew <andrew.gutierrez@stardust.gg>
2023-03-07 16:13:17 -0800

    add usdc test

--- test/blockchain.test.ts
+++ test/blockchain.test.ts
@@ -74,4 +74,43 @@ describe('Blockchain integration tests', () => {
     const finalBalance = hre.ethers.utils.formatEther(await recipient.getBalance());
     expect(finalBalance - initialBalance).toEqual(sendValue);
   });
+
+  it('should be able to interact with usdc contract to send usdc', async () => {
+    const sdk = new StardustCustodialSDK(apiKey);
+    const wallet = await sdk.getWallet(walletId);
+    const signer = wallet.signers.ethers.connect(hardhatEthersProvider);
+
+    const usdcAddress = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174';
+
+    // ABI of the USDC token contract
+    const usdcAbi = [
+      'function balanceOf(address account) view returns (uint256)',
+      'function transfer(address recipient, uint256 amount) returns (bool)',
+    ];
+
+    // Create an instance of the USDC token contract
+    const usdcContract = new hre.ethers.Contract(usdcAddress, usdcAbi, signer);
+    const myAddress = await signer.getAddress();
+    const usdcXfer = '100';
+    const usdcWhale = '0x33Aa0f00B75Bf69543f1351412FFF1e3A12D114a';
+    const usdcWhaleBalance = await usdcContract.balanceOf(usdcWhale);
+    expect(usdcWhaleBalance.toNumber()).toBeGreaterThan(1000);
+    const impersonatedSigner = await hre.ethers.getImpersonatedSigner(usdcWhale);
+    const impersonatedContract = new hre.ethers.Contract(usdcAddress, usdcAbi, impersonatedSigner);
+    await impersonatedContract.transfer(myAddress, hre.ethers.utils.parseUnits(usdcXfer, 6));
+    const myBalanceAfter = await usdcContract.balanceOf(myAddress);
+    expect(myBalanceAfter.toNumber()).toEqual(hre.ethers.utils.parseUnits(usdcXfer, 6).toNumber());
+
+    // now our legit signer has some usdc. Lets use our signer to send the usdc to another address
+    // Transfer 10 USDC to another address
+    const recipient2 = hre.ethers.Wallet.createRandom().connect(hardhatEthersProvider);
+    const usdcXferAgain = '10';
+    const amount = hre.ethers.utils.parseUnits(usdcXferAgain, 6);
+    const tx = await usdcContract.transfer(recipient2.address, amount);
+    await tx.wait(1);
+    const recipientBalance = await usdcContract.balanceOf(recipient2.address);
+    expect(recipientBalance.toNumber()).toEqual(
+      hre.ethers.utils.parseUnits(usdcXferAgain, 6).toNumber()
+    );
+  });
 });
